d4df2d0679f3b7c120bd4c57729b0822bae3f228
Andrew Lukichev <andrewlukichev@gmail.com>
2016-04-06 13:48:51 +0300

    [DEV] aside menu

--- front/src/base/ng-clip.js
+++ front/src/base/ng-clip.js
@@ -1,67 +1,74 @@
 angular
     .module('ebil.base')
+    .factory('clipboard', clipboardFactory)
+    .directive('clipboard', clipboardDirective);
 
-    .factory('clipboard', ['$document', function ($document) {
-        function createNode(text) {
-            var node = $document[0].createElement('textarea');
-            node.style.position = 'absolute';
-            node.style.left = '-10000px';
-            node.textContent = text;
-            return node;
+clipboardFactory.$inject = ['$document'];
+
+function clipboardFactory($document) {
+    function createNode(text) {
+        var node = $document[0].createElement('textarea');
+        node.style.position = 'absolute';
+        node.style.left = '-10000px';
+        node.textContent = text;
+        return node;
+    }
+
+    function copyNode(node) {
+        try {
+            // Set inline style to override css styles
+            $document[0].body.style.webkitUserSelect = 'initial';
+
+            var selection = $document[0].getSelection();
+            selection.removeAllRanges();
+            node.select();
+
+            if(!$document[0].execCommand('copy')) {
+                throw('failure copy');
+            }
+            selection.removeAllRanges();
+        } finally {
+            // Reset inline style
+            $document[0].body.style.webkitUserSelect = '';
         }
+    }
 
-        function copyNode(node) {
+    function copyText(text) {
+        var node = createNode(text);
+        $document[0].body.appendChild(node);
+        copyNode(node);
+        $document[0].body.removeChild(node);
+    }
+
+    return {
+        copyText: copyText
+    };
+}
+
+clipboardDirective.$inject = ['clipboard'];
+function clipboardDirective(clipboard) {
+    return {
+        restrict: 'A',
+        scope: {
+            onCopied: '&',
+            onError: '&',
+            text: '='
+        },
+        link : link
+    };
+
+    function link(scope, element) {
+        element.on('click', function (event) {
             try {
-                // Set inline style to override css styles
-                $document[0].body.style.webkitUserSelect = 'initial';
-
-                var selection = $document[0].getSelection();
-                selection.removeAllRanges();
-                node.select();
-
-                if(!$document[0].execCommand('copy')) {
-                    throw('failure copy');
+                clipboard.copyText(scope.text);
+                if (angular.isFunction(scope.onCopied)) {
+                    scope.$evalAsync(scope.onCopied());
                 }
-                selection.removeAllRanges();
-            } finally {
-                // Reset inline style
-                $document[0].body.style.webkitUserSelect = '';
+            } catch (err) {
+                if (angular.isFunction(scope.onError)) {
+                    scope.$evalAsync(scope.onError({err: err}));
+                }
             }
-        }
-
-        function copyText(text) {
-            var node = createNode(text);
-            $document[0].body.appendChild(node);
-            copyNode(node);
-            $document[0].body.removeChild(node);
-        }
-
-        return {
-            copyText: copyText
-        };
-    }])
-
-    .directive('clipboard', ['clipboard', function (clipboard) {
-        return {
-            restrict: 'A',
-            scope: {
-                onCopied: '&',
-                onError: '&',
-                text: '='
-            },
-            link: function (scope, element) {
-                element.on('click', function (event) {
-                    try {
-                        clipboard.copyText(scope.text);
-                        if (angular.isFunction(scope.onCopied)) {
-                            scope.$evalAsync(scope.onCopied());
-                        }
-                    } catch (err) {
-                        if (angular.isFunction(scope.onError)) {
-                            scope.$evalAsync(scope.onError({err: err}));
-                        }
-                    }
-                });
-            }
-        };
-    }]);
+        });
+    }
+}
